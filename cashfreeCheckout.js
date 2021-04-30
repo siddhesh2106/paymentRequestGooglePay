8cddc4df1f17eb7a186d11f6f791e6ea0ded9cb7
siddhesh2106 <28688567+siddhesh2106@users.noreply.github.com>
2021-04-30 16:21:56 +0530

    Update cashfreeCheckout.js

--- cashfreeCheckout.js
+++ cashfreeCheckout.js
@@ -1,6 +1,10 @@
 
 function onBuyClicked(){
   console.log("buy button is clicked");
+  var form = document.createElement("form");
+  form.setAttribute("method", "post");
+  form.setAttribute("action", "https://test.cashfree.com/billpay/checkout/post/submit");
+  
 }
 
 function onSellClicked(){
